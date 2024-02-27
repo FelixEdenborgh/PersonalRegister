4f663c996f561dcd9947ba999f3c7059472aedc1
FelixEdenborgh <Felix@edenborgh.se>
2024-02-27 14:35:34 +0100

    Add project files.

---
+++ PersonalRegister/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PersonalRegister
+{
+
+    class Personalregister
+    {
+        public string Name { get; set; }
+        public int Salary {  get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        // En lista
+        // Behöver en method för add
+        // behöver en while loop med if else eller switch för att välja vad som ska komma in samt exit
+        // Behöver en method för att skriva ut hela listan
+    }
+}
