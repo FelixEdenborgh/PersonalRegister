7de33b0f5aba9e0b53493b5b5667674b17e6ce95
FelixEdenborgh <Felix@edenborgh.se>
2024-02-27 15:04:09 +0100

    Added function to add user, and show all users

--- PersonalRegister/Program.cs
+++ PersonalRegister/Program.cs
@@ -6,16 +6,66 @@
         public string Name { get; set; }
         public int Salary {  get; set; }
     }
-    internal class Program
+    class Program
     {
+        static List<Personalregister> people = new List<Personalregister>();
         static void Main(string[] args)
         {
             
+
+            int choice, salary;
+            string name;
+            bool run = true;
+            
+            while(run) 
+            {
+                Console.WriteLine("Hey and welcome to this Register");
+                Console.WriteLine("Choice your option: ");
+                Console.WriteLine("1. Add Name & Salary");
+                Console.WriteLine("2. Show All");
+                Console.WriteLine();
+                Console.WriteLine("Every other input will result in EXIT!");
+                choice = Convert.ToInt32(Console.ReadLine());
+                
+
+                if (choice == 1)
+                {
+                    // add user
+                    Console.WriteLine("Name of user: ");
+                    name = Console.ReadLine();
+                    Console.WriteLine("How much should the user have in Salary: ");
+                    salary = Convert.ToInt32(Console.ReadLine());
+
+                    AddUserToList(name, salary);
+
+                }
+                else if (choice == 2)
+                {
+                    // view all users
+                    ShowUsersData();
+                }
+                else
+                {
+                    run = false;
+                }
+            }
+            
         }
 
-        // En lista
-        // Behöver en method för add
-        // behöver en while loop med if else eller switch för att välja vad som ska komma in samt exit
-        // Behöver en method för att skriva ut hela listan
+        static void AddUserToList(string name, int Salary)
+        {
+            Personalregister newUser = new Personalregister { Name = name, Salary = Salary };
+            people.Add(newUser);
+        }
+
+        static void ShowUsersData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("All persons in the register: ");
+            foreach (Personalregister person in people)
+            {
+                Console.WriteLine($"Name: {person.Name}, Salary: {person.Salary} $");
+            }
+        }
     }
 }
